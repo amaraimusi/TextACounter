fbc22ace05084c3fe258e8e4f709d69a3cf51d87
k-uehara <k-uehara@se-ec.co.jp>
2016-11-29 18:43:13 +0900

    Currently under development.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TextACounter
 Count the number of characters in the text area and display it on another element.
+
+# Example
+<http://amaraimusi.sakura.ne.jp/TextACounter/example1.html>
+
+
---
+++ TextACounter.js
@@ -0,0 +1,11 @@
+/**
+ * TextACounter.js
+ * 
+ * @note
+ * Count the number of characters in the text area and display it on another element.
+ * 
+ * @version 1.0
+ * @date 2016-11-29
+ * @auther kenji uehara
+ * 
+ */
