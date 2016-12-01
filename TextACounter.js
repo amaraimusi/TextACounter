0a6f2d2a67671bc06b4ace55421245c611954942
k-uehara <k-uehara@se-ec.co.jp>
2016-12-01 14:11:16 +0900

    Relese version 1.2

--- TextACounter.js
+++ TextACounter.js
@@ -4,7 +4,7 @@
  * @note
  * Count the number of characters in the text area and display it on another element.
  * 
- * @version 1.1
+ * @version 1.2
  * @date 2016-11-29 | 2016-12-1
  * @auther kenji uehara
  * 
@@ -12,6 +12,8 @@
 var TextACounter =function(option){
 
 	var myself=this; // Instance of myself.
+	
+	this.elmsList = [];
 
 	/**
 	 * initialized.
@@ -32,14 +34,38 @@ var TextACounter =function(option){
 		var ta = $(ta_slt);
 		var disp = $(display_slt);
 		
+		var str = ta.val();
+		var len = str.length;
+		
 		notified(ta,disp);
-		
-		
-		
+
 		ta.keyup(function(e){
 			notified(ta,disp);
 		});
 		
+		var elms = {
+				'ta':ta,
+				'disp':disp,
+		};
+		
+		this.elmsList.push(elms)
+		
+	};
+	
+	
+	/**
+	 * Refresh the then count display elements.
+	 * 
+	 * @note 
+	 * When the text area is rewritten by another program.<br>
+	 * In this case, the "refresh" function.
+	 */
+	this.refresh = function(){
+		for(var i in this.elmsList){
+			var elms = this.elmsList[i];
+			
+			notified(elms.ta,elms.disp);
+		}
 	};
 	
 	/**
@@ -50,7 +76,7 @@ var TextACounter =function(option){
 	function notified(ta,disp){
 		var str = ta.val();
 		var len = str.length;
-	
+		
 		var maxlength = ta.attr('maxlength');
 		if(!maxlength){
 			disp.html(len); 
